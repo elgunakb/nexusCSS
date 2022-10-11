d9b1b791a250ee4c2955d327325c8d0baa0caa0f
Elgun Akbarli <elgunekberli200@gmail.com>
2022-10-11 15:23:19 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function toggle() {
+    const sidebar = document.getElementById("navDivDisplay");
+    const visibleClassName = 'visible';
+  
+    if (sidebar.classList.contains(visibleClassName)) {
+      sidebar.classList.remove(visibleClassName);
+    } else {
+      sidebar.classList.add(visibleClassName);
+    }
+  }
